1595dc0638ebb14e209252bc557a847311a5b13f
Shizhong Huang <remir8801@gmail.com>
2018-07-16 14:09:05 +0800

    the boundaries!!

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -4,13 +4,15 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
-        if (len(strs)<=1 or len(strs[0])==0):
+        if (len(strs) == 0):
             return ""
+        elif (len(strs) == 1):
+            return strs[0]
         j = 0
         flag = True
         while flag:
             for i in range(1,len(strs)):
-                if (j>=len(strs[i]) or strs[0][j] != strs[i][j]):
+                if (j>=len(strs[i]) or j>=len(strs[0]) or strs[0][j] != strs[i][j]):
                     flag = False
                     break
             j = j+1
