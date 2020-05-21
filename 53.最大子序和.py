8d3c919b8ecb81a22a1af84bb6811e6a91289579
remir8801 <remir8801@gmail.com>
2020-05-21 10:17:28 +0800

    greedy

---
+++ 53.最大子序和.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=53 lang=python3
+#
+# [53] 最大子序和
+#
+
+# @lc code=start
+
+class Solution:               
+    def maxSubArray(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[i] = max(0, nums[i-1]) + nums[i]
+        return max(nums)
+
+# @lc code=end
+
